99a030acf93c15a7a912e0c320821c153fba5133
Thomas Frøysa <thomasdf@stud.ntnu.no>
2016-10-04 15:47:06 +0200

    setup

---
+++ SciChartTest/DataCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace SciChartTest {
+	internal class DataCreator {
+		MainWindow Recepient;
+		int timeOut;
+		int dataCount;
+
+		public DataCreator(MainWindow sender, int timeOut, int dataCount) {
+			Recepient = sender;
+			this.timeOut = timeOut;
+			this.dataCount = dataCount;
+		}
+
+		public async void createData(int timeout, int dataCount = 1000) {
+			double data;
+			for(int i = 0; i < dataCount; i++) {
+				data = Math.Sin(2 * Math.PI * i / dataCount);
+				Thread.Sleep(timeOut);
+            }
+        }
+	}
+}
---
+++ SciChartTest/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using SciChart.Charting.Model.DataSeries;
+using SciChart.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SciChartTest {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+
+		XyDataSeries<double, double> series = new XyDataSeries<double, double>();
+
+		public MainWindow() {
+			Loaded += MainWindow_OnLoaded;
+            InitializeComponent();
+		}
+
+		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e) {
+			// GrowBy expands a gap around the data when zooming to extents
+			sciChartSurface.DebugWhyDoesntSciChartRender = false;
+			sciChartSurface.YAxis.GrowBy = new DoubleRange(0.2, 0.2);
+
+			// VisibleRange may be set explicitly, or use AutoRange to zoom to extents
+			sciChartSurface.XAxis.VisibleRange = new DoubleRange(0, 100000);
+			sciChartSurface.YAxis.VisibleRange = new DoubleRange(-2, 2);
+
+			// Add a data series and some data
+			series.SeriesName = "Sinewave";
+			series.AcceptsUnsortedData = true;
+
+			// Note: Can also be set in Xaml, inside the tag
+			lineSeries.DataSeries = series;
+			for(int i = 0; i < 100000; i += 100) {
+				produceData(10, i, 100000);
+			}
+
+		}
+
+		public async void produceData(int timeout, double index, int totalcount) {
+			double[] dataArray = await createData(timeout, index, totalcount);
+			for(int i = 0; i<dataArray.Length; i++) {
+				series.Append(index + i, dataArray[i]);
+			}
+		}
+
+		public Task<double[]> createData(int timeout, double index, int totalcount) {
+			return Task.Run(() => {
+				Thread.Sleep(timeout);
+				double[] resultArray = new double[100];
+				for(int i = 0; i < 100; i++) {
+					resultArray[i] = Math.Sin(2 * Math.PI * (index+i) / totalcount);
+				}
+				return resultArray;
+			});
+		}
+	}
+}
